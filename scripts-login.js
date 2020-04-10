7185cb5054a810b251b6e30372470006d5b2ed70
Nina Ruiz <nina.ruizandres@gmail.com>
2020-04-10 14:13:38 +0200

    Add password and email imput constants inside loginButton function

--- scripts-login.js
+++ scripts-login.js
@@ -1,11 +1,10 @@
-const password = $('#login-password');
-const email = $('#login-email');
-
 const loginButton = $('#login-button');
 
 const login = () => {
     loginButton.click(function (e) {
         e.preventDefault();
+        const password = $('#login-password');
+        const email = $('#login-email');
         const enterEmail = email[0].value;
         const enterPassword = password[0].value;
         console.log(enterPassword, enterEmail);
